86d7242059982aac4ff57fab799377ecde92111b
Hamit Mızrak <hamitmizrak@gmail.com>
2023-09-17 18:29:32 +0300

    Success

--- src/main/java/com/hamitmizrak/assist/FrontendUrl.java
+++ src/main/java/com/hamitmizrak/assist/FrontendUrl.java
@@ -2,5 +2,6 @@ package com.hamitmizrak.assist;
 
 public class FrontendUrl {
 
-    public static final String REACT_URL="http://localhost:3000";
+   /* public static final String REACT_URL="http://localhost:3000";*/
+    public static final String REACT_URL="host.docker.internal:3000";
 }
